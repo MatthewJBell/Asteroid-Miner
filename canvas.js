a07fee52ad8d6f8d795f15dd3ede6aeb0b784951
MatthewJBell <mattjbell97@gmail.com>
2019-02-04 19:58:32 -0500

    added canvas.js

---
+++ canvas.js
@@ -0,0 +1,96 @@
+console.log('Asteroid miner');
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+c.fillStyle = 'rgba(255, 0, 0, 0.1)';
+c.fillRect(100, 100, 100, 100);
+c.fillRect(100, 200, 100, 100);
+c.fillStyle = 'rgba(0, 255, 0, 0.1)';
+c.fillRect(100, 400, 100, 100);
+console.log(canvas);
+
+//line
+c.beginPath();
+c.moveTo(50,300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = "#fa34a3";
+c.stroke();
+
+//arc / circle
+// c.arc(300, 300, 30, 0, Math.PI * 2, false);
+// c.strokeStyle = 'blue';
+// c.stroke();
+
+// for(var i = 0; i < 3000; i++) {
+//     var x = Math.random() * window.innerWidth;
+//     var y = Math.random() * window.innerHeight;
+//     c.beginPath();
+//     c.arc(x, y, 30, 0, Math.PI * 2, false);
+//     c.strokeStyle = 'blue';
+//     c.stroke();
+// }
+
+// c.beginPath();
+// c.arc(200, 200, 30, 0, Math.PI * 2, false);
+// c.strokeStyle = 'blue';
+// // c.stroke();
+// var x = Math.random() * innerWidth;
+// var x = Math.random() * innerHeight;
+// var y = 200;
+// var dx = (Math.random() - 0.5) * 20;
+// var dy = (Math.random() - 0.5) * 20;
+// var radius = 30;
+
+function Circle(x, y, dx, dy, radius){
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+
+    this.draw = function(){
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = 'blue';
+        c.stroke();
+    }
+    this.update = function(){
+        if(this.x + this.radius > innerWidth || this.x - radius < 0) {
+            this.dx = -this.dx;
+        }
+    
+        if(this.y + this.radius > innerHeight || this.y- radius < 0) {
+            this.dy = -this.dy;
+        }
+    
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
+}
+
+var circle = new Circle(200, 200, 3, 3, 30);
+
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+
+    circle.update();
+
+    c.beginPath();
+    c.arc(x, y, radius, 0, Math.PI * 2, false);
+    c.strokeStyle = 'blue';
+    c.stroke();
+
+
+}
+
+animate();
